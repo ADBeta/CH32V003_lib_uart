fa08c80febc6a86fe847e98fed28ef66e317f144
ADBeta <ashley.adbeta@gmail.com>
2024-08-03 14:01:54 +0100

    UART Working, all Bauds tested

--- testing/uarttest.c
+++ testing/uarttest.c
@@ -5,10 +5,22 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
 
+// Define some commonly used baus rates
+// DIV = (HCLK / (16 * BAUD)) * 16 (HCLK is 48MHz)
+typedef enum {
 
-// TODO: predefine UART speeds
-
-#define USART_HalfDuplex 0x00000008
+	UART_BAUD_921600 = 0x0034,    // 923076    
+	UART_BAUD_460800 = 0x0068,    // 461538
+	UART_BAUD_230400 = 0x00D3,    // 227488
+	UART_BAUD_115200 = 0x01A1,    // 115107
+	UART_BAUD_57600  = 0x0341,    // 57623
+	UART_BAUD_38400  = 0x04E2,    // 38400
+	UART_BAUD_19200  = 0x09C4,    // 19200
+	UART_BAUD_9600   = 0x1388,    // 9600
+	UART_BAUD_4800   = 0x2710,    // 4800
+	UART_BAUD_2400   = 0x4E20,    // 2400
+	UART_BAUD_1200   = 0x9C40,    // 1200
+} UART_BAUDRATE_DEF;
 
 
 int main()
@@ -23,34 +35,25 @@ int main()
 	gpio_set_mode(GPIO_PD6, INPUT_FLOATING);
 
 	// 115200, 8n1.  Note if you don't specify a mode, UART remains off even when UE_Set.
-	USART1->CTLR1 = USART_WordLength_8b | USART_Parity_No | USART_Mode_Tx;
+	USART1->CTLR1 = USART_WordLength_8b | USART_Parity_No | USART_Mode_Tx | USART_Mode_Rx;
 	USART1->CTLR2 = USART_StopBits_1;
-	USART1->CTLR3 = 0x00; // USART_HalfDuplex;
+	USART1->CTLR3 = 0x00;
 
-	USART1->BRR = UART_BRR;
+	USART1->BRR = UART_BAUD_1200;
 	USART1->CTLR1 |= CTLR1_UE_Set;
 
-/*
-	char str[32] = "Hello World!!\r\n";
-	while(1)
-	{
-		for(int i = 0; i < 16; i++){
-	    	while( !(USART1->STATR & USART_FLAG_TC));
-	    	USART1->DATAR = str[i];
-		}
 
-		Delay_Ms(100);
-	}
-*/
 	while(1)
 	{
-		if(USART1->STATR & USART_FLAG_RXNE)
+		if( (USART1->STATR & USART_FLAG_RXNE) != 0)
 		{
-			printf("data: ");
-			char c = USART1->DATAR;
+			char c = (char)USART1->DATAR;
 			printf("%c", c);
+
+			while( !(USART1->STATR & USART_FLAG_TC));
+	    	USART1->DATAR = c;
 		}
 
-	}
+	}  
 	
 }
